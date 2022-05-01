b78c69fb20663456fb85e698895dcb2466cec2b2
sakurafisch <sakurafisch@gmail.com>
2022-05-01 20:55:37 +0800

    feat: build a proof of existence dApp

--- src/TemplateModule.js
+++ src/TemplateModule.js
@@ -1,79 +1,116 @@
-import React, { useEffect, useState } from 'react'
-import { Form, Input, Grid, Card, Statistic } from 'semantic-ui-react'
+import React, { useEffect, useState } from 'react';
+import { Form, Input, Grid, Message } from 'semantic-ui-react';
 
-import { useSubstrateState } from './substrate-lib'
-import { TxButton } from './substrate-lib/components'
+// Pre-built Substrate front-end utilities
+// for connecting to a node and making a transaction.
+import { useSubstrateState } from './substrate-lib';
+import { TxButton } from './substrate-lib/components';
 
-function Main(props) {
-  const { api } = useSubstrateState()
+// Polkadot-JS utilities for hashing data.
+import { blake2AsHex } from '@polkadot/util-crypto';
 
-  // The transaction submission status
-  const [status, setStatus] = useState('')
+// Main Proof Of Existence component
+const Main = (props) => {
+    // Establish an API to talk to the Substrate node.
+    const { api, currentAccount } = useSubstrateState();
+    // React hooks for all the state variables we track.
+    const [status, setStatus] = useState('');
+    const [digest, setDigest] = useState('');
+    const [owner, setOwner] = useState('');
+    const [block, setBlock] = useState(0);
 
-  // The currently stored value
-  const [currentValue, setCurrentValue] = useState(0)
-  const [formValue, setFormValue] = useState(0)
+    let fileReader;
+    const bufferToDigest = () => {
+        const content = Array.from(new Uint8Array(fileReader.result))
+            .map(b => b.toString(16).padStart(2, '0'))
+            .join('');
+        
+        const hash = blake2AsHex(content, 256);
+        setDigest(hash);
+    };
 
-  useEffect(() => {
-    let unsubscribe
-    api.query.templateModule
-      .something(newValue => {
-        // The storage value is an Option<u32>
-        // So we have to check whether it is None first
-        // There is also unwrapOr
-        if (newValue.isNone) {
-          setCurrentValue('<None>')
-        } else {
-          setCurrentValue(newValue.unwrap().toNumber())
-        }
-      })
-      .then(unsub => {
-        unsubscribe = unsub
-      })
-      .catch(console.error)
+    const handleFileChosen = file => {
+        fileReader = new FileReader();
+        fileReader.onloadend = bufferToDigest;
+        fileReader.readAsArrayBuffer(file);
+    };
 
-    return () => unsubscribe && unsubscribe()
-  }, [api.query.templateModule])
+    useEffect(() => {
+        let unsubscribe;
 
-  return (
-    <Grid.Column width={8}>
-      <h1>Template Module</h1>
-      <Card centered>
-        <Card.Content textAlign="center">
-          <Statistic label="Current Value" value={currentValue} />
-        </Card.Content>
-      </Card>
-      <Form>
-        <Form.Field>
-          <Input
-            label="New Value"
-            state="newValue"
-            type="number"
-            onChange={(_, { value }) => setFormValue(value)}
-          />
-        </Form.Field>
-        <Form.Field style={{ textAlign: 'center' }}>
-          <TxButton
-            label="Store Something"
-            type="SIGNED-TX"
-            setStatus={setStatus}
-            attrs={{
-              palletRpc: 'templateModule',
-              callable: 'doSomething',
-              inputParams: [formValue],
-              paramFields: [true],
-            }}
-          />
-        </Form.Field>
-        <div style={{ overflowWrap: 'break-word' }}>{status}</div>
-      </Form>
-    </Grid.Column>
-  )
+        api.query.templateModule
+            .proofs(digest, result => {
+                if (result.inspect().inner) {
+                    let [tmpAddress, tmpBlock] = result.toHuman()
+                    setOwner(tmpAddress);
+                    setBlock(tmpBlock);
+                } else {
+                    setOwner('');
+                    setBlock(0);
+                }
+            })
+            .then(unsub => {
+                unsubscribe = unsub;
+            });
+        return () => unsubscribe && unsubscribe();
+    }, [digest, api.query.templateModule]);
+
+    const isClaimed = () => {
+        return block !== 0;
+    }
+
+    return (
+        <Grid.Column>
+            <h1>Proof of Existence</h1>
+            <Form success={!!digest && !isClaimed()}>
+                <Form.Field>
+                    <Input
+                        type="file"
+                        id="file"
+                        label="Your File"
+                        onChange={e => handleFileChosen(e.target.files[0])}
+                    />
+                    <Message
+                        warning
+                        header="File Digest Claimed"
+                        list={[digest, `Owner: ${owner}`, `Block: ${block}`]}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <TxButton
+                        label={"Create Claim"}
+                        type="SIGNED-TX"
+                        setStatus={setStatus}
+                        disabled={isClaimed() || !digest}
+                        attrs={{
+                            palletRpc: 'templateModule',
+                            callable: 'createClaim',
+                            inputParams: [digest],
+                            paramFields: [true]
+                        }}
+                    />
+                    <TxButton
+                        label={"Revoke Claim"}
+                        type="SIGNED-TX"
+                        setStatus={setStatus}
+                        disabled={!isClaimed() || owner !== currentAccount.address}
+                        attrs={{
+                            palletRpc: 'templateModule',
+                            callable: 'revokeClaim',
+                            inputParams: [digest],
+                            paramFields: [true]
+                        }}
+                    />
+                </Form.Field>
+                <div style={{ overflowWrap: 'break-word' }}>{status}</div>
+            </Form>
+        </Grid.Column>
+    );
 }
 
-export default function TemplateModule(props) {
-  const { api } = useSubstrateState()
-  return api.query.templateModule && api.query.templateModule.something ? (
-    <Main {...props} />
-  ) : null
+const TemplateModule = (props) => {
+    const { api } = useSubstrateState();
+    return api.query.templateModule ? <Main {...props} /> : null;
 }
+
+export default TemplateModule;
